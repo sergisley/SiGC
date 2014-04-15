a5d745624fe5e445c2fde011bf75ec38c6dd97cf
Sergisley <sergisley@gmail.com>
2014-04-15 17:14:18 -0300

    teste banco rodando
    !!!!!!!!!!!!!

--- config/application.config.php
+++ config/application.config.php
@@ -3,9 +3,7 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
-        'ZendDeveloperTools-master',
-        'BjyProfiler-master',
-        
+                
     ),
 
     // These are various options for the listeners attached to the ModuleManager
--- module/Application/src/Application/Controller/ChamadoController.php
+++ module/Application/src/Application/Controller/ChamadoController.php
@@ -25,14 +25,75 @@ class ChamadoController extends AbstractActionController
     {
         return new ViewModel();
     }
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  
+    
+          /**
+ * action index
+ * @return \Zend\View\Model\ViewModel
+ */
+
     public function listarchamadosAction()
     {
+ 
+    /**
+     * função anônima para var_dump estilizado
+     */
+    $myVarDump = function($nome_linha = "Nome da Linha", $data = null, $caracter = ' - ') {
+                echo str_repeat($caracter, 100) . '<br/>' . ucwords($nome_linha) . '<pre><br/>';
+                var_dump($data);
+                echo '</pre>' . str_repeat($caracter, 100) . '<br/><br/>';
+            };
+ 
+    /**
+     * conexão com banco
+     */
+    $adapter = new \Zend\Db\Adapter\Adapter(array(
+        'driver'    => 'Pdo_Mysql',
+        'database'  => 'sigc_db',
+        'username'  => 'root',
+        'password'  => 'root'
+    ));
+ 
+    /**
+     * obter nome do sehema do nosso banco
+     */
+    $myVarDump(
+            "Nome Schema", 
+            $adapter->getCurrentSchema()
+    );
+ 
+    /**
+     * contar quantidade de elementos da nossa tabela
+     */
+    $myVarDump(
+            "Quantidade elementos tabela chamados", 
+            $adapter->query("SELECT * FROM `chamado`")->execute()->count()
+    );
+ 
+    /**
+     * montar objeto sql e executar
+     */
+    $sql        = new \Zend\Db\Sql\Sql($adapter);
+    $select     = $sql->select()->from('chamado');
+    $statement  = $sql->prepareStatementForSqlObject($select);
+    $resultsSql = $statement->execute();
+    $myVarDump(
+            "Objet Sql com Select executado",
+            $resultsSql
+    );
+ 
+    /**
+     * motar objeto resultset com objeto sql e mostrar resultado em array
+     */
+    $resultSet = new \Zend\Db\ResultSet\ResultSet;
+    $resultSet->initialize($resultsSql);
+    $myVarDump(
+            "Resultado do Objeto Sql para Array ",
+            $resultSet->toArray()
+    );
+    die(); 
         
         
-        
-        
-        return new ViewModel();
         
     }
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
